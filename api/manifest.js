a5af5c4b0689b3c02d33f619cec2a736ca678ef7
isaacronydayan <isaacronydayan@gmail.com>
2026-07-07 20:29:44 -0300

    Delete api directory

--- api/manifest.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/manifest+json');
-  res.status(200).json({
-    name: 'Isaac OS', short_name: 'Isaac OS', start_url: '/', display: 'standalone',
-    background_color: '#08090b', theme_color: '#08090b',
-  });
-}
